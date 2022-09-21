52949c7d31518efa77e9967d22725e03b61bb1bd
29venom1 <29venom1@gmail.com>
2022-09-21 23:38:39 +0300

    added maxsearch_recursion.py and quicksort

--- SelectionSort_1.py
+++ SelectionSort_1.py
@@ -5,6 +5,7 @@ def find_smallest(arr):
     smallest = arr[0]
     smallest_index = 0
     for i in range(1, len(arr)):
+
         if arr[i] < smallest:
             smallest = arr[i]
             smallest_index = i
@@ -12,7 +13,7 @@ def find_smallest(arr):
 
 
 def selection_sort(arr):
-    new_arr = array('i',[])
+    new_arr = array('i', [])
     for i in range(len(arr)):
         smallest = find_smallest(arr)
         new_arr.append(arr.pop(smallest))
